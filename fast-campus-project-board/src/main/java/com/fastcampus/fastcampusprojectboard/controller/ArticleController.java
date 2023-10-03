037256654e0f8a325a08dd7ca7056dbb705d07a4
sunyongoh <royce15011@gmail.com>
2023-10-03 11:43:01 +0900

    #28 - 페이지네이션 기능을 컨트롤러(게시판 뷰) 에 반영
    페이징 정렬 테스트 나중에 정렬 구현에서 사용할 것

--- fast-campus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
+++ fast-campus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.fastcampus.fastcampusprojectboard.domain.type.SearchType;
 import com.fastcampus.fastcampusprojectboard.response.ArticleResponse;
 import com.fastcampus.fastcampusprojectboard.response.ArticleWithCommentsResponse;
 import com.fastcampus.fastcampusprojectboard.service.ArticleService;
+import com.fastcampus.fastcampusprojectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -16,12 +18,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -30,7 +35,11 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ){
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue,pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue,pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers =paginationService.getPaginationBarNumbers(pageable.getPageNumber(),articles.getTotalPages());
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
